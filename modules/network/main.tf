771e1ebcb41c448ea83d0febe81bc1eeccad536c
Sanjay <scshitole@gmail.com>
2022-11-09 19:07:11 -0800

    update core

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "bigip" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.1/16"]
+    cidr_blocks = ["192.10.0.0/16"]
   }
 
   egress {
