3e54eb5344ba3d929523ce8ecfb0bac829fcf80f
Sanjay <scshitole@gmail.com>
2023-01-25 12:04:44 -0800

    update to default

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "bigip" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["192.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
